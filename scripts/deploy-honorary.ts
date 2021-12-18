f9ea98c0c7e56f947357fb1fe315db5ba2933854
Talha Akkaya <talha2akkaya@gmail.com>
2021-12-18 13:50:57 -0500

    lottery added

--- scripts/deploy-honorary.ts
+++ scripts/deploy-honorary.ts
@@ -26,6 +26,27 @@ async function main() {
   const honorary = await _.deploy();
   await honorary.deployed();
 
+  const honoraryTokens = [
+    {
+      account: await owner.getAddress(),
+      tokenURI: "ipfs://QmQ9CCka6GyqbqVRXHfz3hwn2AvcDZA5QM4Nnn7wk6heeL",
+    },
+    {
+      account: await owner.getAddress(),
+      tokenURI: "ipfs://QmaGW4vJi8hs2phv3nuZyCkDE2LFbiDwQjMV2cgEB7CnPN",
+    },
+  ];
+
+  for (let i = 0; honoraryTokens.length > i; i++) {
+    const tx = await honorary.safeMint(
+      honoraryTokens[i].account,
+      honoraryTokens[i].tokenURI
+    );
+    await tx.wait();
+    process.stdout.write(`\r  > Minted: ${i}`);
+  }
+  console.log("");
+
   const honoraryABIResponse = await pinJSONToIPFS(
     process.env.PINATA_API_KEY as string,
     process.env.PINATA_API_SECRET as string,
