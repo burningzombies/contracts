55ec88a97c4ed35d034c298498b2ef53d780409e
Talha Akkaya <talha2akkaya@gmail.com>
2022-01-02 10:32:46 -0500

    organized

--- contracts/BurningZombiesERC721.sol
+++ contracts/BurningZombiesERC721.sol
@@ -19,6 +19,8 @@ contract BurningZombiesERC721 is
 {
     using Counters for Counters.Counter;
 
+    /* ========== STATE VARIABLES ========== */
+
     uint256 public constant MAX_SUPPLY = 3024;
     uint256 public constant MAX_TOKEN_PER_TX = 24;
     uint256 public constant MAX_TOKEN_PER_WALLET = 100;
@@ -46,7 +48,12 @@ contract BurningZombiesERC721 is
     mapping(uint256 => uint256) private _lastDividendAt;
     mapping(uint256 => address) private _minters;
 
-    event BurnedReflectionDivided(address indexed caller);
+    event BurnedReflectionDivided(
+        address indexed user,
+        uint256 indexed tokenId
+    );
+
+    /* ========== CONSTRUCTOR ========== */
 
     constructor(
         address splitter_,
@@ -65,32 +72,6 @@ contract BurningZombiesERC721 is
         reflectionStep = 0;
         priceBase = 1.5 ether;
         priceStep = 0;
-    }
-
-    function setReflectionDynamics(
-        uint256 segmentSize_,
-        uint256 reflectionBase_,
-        uint256 reflectionStep_,
-        uint256 priceBase_,
-        uint256 priceStep_
-    ) public onlyOwner {
-        require(
-            _tokenIdCounter.current() == 0,
-            "BurningZombiesERC721: first token already minted"
-        );
-        segmentSize = segmentSize_;
-        reflectionBase = reflectionBase_;
-        reflectionStep = reflectionStep_;
-        priceBase = priceBase_;
-        priceStep = priceStep_;
-    }
-
-    function setPriceCalculator(address address_) external onlyOwner {
-        _priceCalculator = IPriceCalculator(address_);
-    }
-
-    function setMintingShareSplitter(address address_) external onlyOwner {
-        _splitter = address_;
     }
 
     function _beforeTokenTransfer(
@@ -122,6 +103,8 @@ contract BurningZombiesERC721 is
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
+    /* ========== VIEWS ========== */
+
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -130,27 +113,6 @@ contract BurningZombiesERC721 is
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
-    }
-
-    function setBaseURI(string memory baseURI) external onlyOwner {
-        _baseURIextended = baseURI;
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return _baseURIextended;
-    }
-
-    function setProvenance(string memory _provenance) public onlyOwner {
-        provenance = _provenance;
-    }
-
-    function _tokenPrice(uint256 tokenId) private view returns (uint256) {
-        uint256 segmentNo = uint256(uint256(tokenId) / uint256(segmentSize));
-        address sender = _msgSender();
-        uint256 price = (segmentNo * priceStep) + priceBase;
-        uint256 balance = balanceOf(sender);
-
-        return _priceCalculator.getPrice(segmentNo, sender, price, balance);
     }
 
     function currentTokenPrice() public view returns (uint256) {
@@ -162,15 +124,8 @@ contract BurningZombiesERC721 is
             tokenId >= 0 && tokenId < MAX_SUPPLY,
             "BurningZombiesERC721: invalid range"
         );
+
         return _tokenPrice(tokenId);
-    }
-
-    function setSaleStart(uint256 timestamp) public onlyOwner {
-        saleStartsAt = timestamp;
-    }
-
-    function setSaleDuration(uint256 duration) public onlyOwner {
-        saleDuration = duration;
     }
 
     function isSaleActive() public view returns (bool) {
@@ -180,19 +135,120 @@ contract BurningZombiesERC721 is
             _tokenIdCounter.current() < MAX_SUPPLY;
     }
 
-    function minterOf(uint256 tokenId) public view returns (address) {
+    function minterOf(uint256 tokenId) external view returns (address) {
         require(
             _exists(tokenId),
             "BurningZombiesERC721: token does not exists"
         );
+
         return _minters[tokenId];
     }
 
-    function currentTokenId() public view returns (uint256) {
+    function currentTokenId() external view returns (uint256) {
         return _tokenIdCounter.current();
     }
 
-    function mintTokens(uint256 numberOfTokens) public payable {
+    function calculateReflectionShare() public view returns (uint256) {
+        if (!(_tokenIdCounter.current() > 1)) return reflectionBase;
+
+        return
+            reflectionBase +
+            ((_tokenIdCounter.current() - 1) / segmentSize) *
+            reflectionStep;
+    }
+
+    function getReflectionBalances() public view returns (uint256) {
+        uint256 count = balanceOf(_msgSender());
+        uint256 total = 0;
+
+        for (uint256 i = 0; count > i; i++) {
+            uint256 tokenId = tokenOfOwnerByIndex(_msgSender(), i);
+            total += getReflectionBalance(tokenId);
+        }
+
+        return total;
+    }
+
+    function getReflectionBalance(uint256 tokenId)
+        public
+        view
+        returns (uint256)
+    {
+        if (!_exists(tokenId)) return 0;
+
+        return totalDividend - _lastDividendAt[tokenId];
+    }
+
+    /* ========== GOVERNOR FUNCTIONS ========== */
+
+    function mintTokensByOwner(uint256 numberOfTokens, address to)
+        external
+        payable
+        onlyOwner
+    {
+        require(isSaleActive(), "BurningZombiesERC721: sale is not active");
+
+        require(
+            numberOfTokens > 0 && numberOfTokens <= MAX_TOKEN_PER_TX,
+            "BurningZombiesERC721: purchase exceeds max limit per transaction"
+        );
+
+        require(
+            _tokenIdCounter.current() + numberOfTokens <= MAX_SUPPLY,
+            "BurningZombiesERC721: purchase exceeds max supply of tokens"
+        );
+
+        _mintTokens(numberOfTokens, to);
+
+        _splitBalance(msg.value);
+    }
+
+    function setReflectionDynamics(
+        uint256 segmentSize_,
+        uint256 reflectionBase_,
+        uint256 reflectionStep_,
+        uint256 priceBase_,
+        uint256 priceStep_
+    ) public onlyOwner {
+        require(
+            _tokenIdCounter.current() == 0,
+            "BurningZombiesERC721: first token already minted"
+        );
+
+        segmentSize = segmentSize_;
+        reflectionBase = reflectionBase_;
+        reflectionStep = reflectionStep_;
+        priceBase = priceBase_;
+        priceStep = priceStep_;
+    }
+
+    function setPriceCalculator(address address_) external onlyOwner {
+        _priceCalculator = IPriceCalculator(address_);
+    }
+
+    function setMintingSharesSplitter(address address_) external onlyOwner {
+        _splitter = address_;
+    }
+
+    function setBaseURI(string memory baseURI) external onlyOwner {
+        _baseURIextended = baseURI;
+    }
+
+    function setProvenance(string memory _provenance) public onlyOwner {
+        provenance = _provenance;
+    }
+
+    function setSaleStart(uint256 timestamp) external onlyOwner {
+        saleStartsAt = timestamp;
+    }
+
+    function setSaleDuration(uint256 duration) external onlyOwner {
+        saleDuration = duration;
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function mintTokens(uint256 numberOfTokens) external payable {
         require(isSaleActive(), "BurningZombiesERC721: sale is not active");
 
         require(
@@ -209,21 +265,14 @@ contract BurningZombiesERC721 is
         for (uint256 i = 0; numberOfTokens > i; i++) {
             amountToPay += _tokenPrice(_tokenIdCounter.current() + i);
         }
+
         require(
             msg.value >= amountToPay,
             "BurningZombiesERC721: ether value sent is not correct"
         );
 
-        for (uint256 i = 0; numberOfTokens > i; i++) {
-            uint256 tokenId = _tokenIdCounter.current();
-            _tokenIdCounter.increment();
+        _mintTokens(numberOfTokens, _msgSender());
 
-            address to = _msgSender();
-
-            _safeMint(to, tokenId);
-            _minters[tokenId] = to;
-            _lastDividendAt[tokenId] = 0;
-        }
         _splitBalance(amountToPay);
     }
 
@@ -232,68 +281,32 @@ contract BurningZombiesERC721 is
             _tokenIdCounter.current() + numberOfTokens <= MAX_SUPPLY,
             "BurningZombiesERC721: burning exceeds max supply of tokens"
         );
+
         require(
             numberOfTokens > 0 && numberOfTokens <= MAX_TOKEN_PER_TX,
             "BurningZombiesERC721: burning exceeds max limit per transaction"
         );
+
         require(!isSaleActive(), "BurningZombiesERC721: sale is active");
 
         for (uint256 i = 0; numberOfTokens > i; i++) {
+            uint256 tokenId = _tokenIdCounter.current();
             _tokenIdCounter.increment();
+
             totalDividend += burnedReflectionBalance / totalSupply();
+
+            emit BurnedReflectionDivided(_msgSender(), tokenId);
         }
-
-        emit BurnedReflectionDivided(_msgSender());
-    }
-
-    function calculateReflectionShare() public view returns (uint256) {
-        if (!(_tokenIdCounter.current() > 1)) return reflectionBase;
-        return
-            reflectionBase +
-            ((_tokenIdCounter.current() - 1) / segmentSize) *
-            reflectionStep;
-    }
-
-    function _splitBalance(uint256 amount) private nonReentrant {
-        uint256 reflectionShare = (amount / 100) * calculateReflectionShare();
-        uint256 mintingShare = amount - reflectionShare;
-        _reflectDividend(reflectionShare);
-
-        address payable recipient = payable(_splitter);
-        Address.sendValue(recipient, mintingShare);
-    }
-
-    function _reflectDividend(uint256 amount) private {
-        reflectionBalance += amount;
-        totalDividend += (amount / MAX_SUPPLY);
-        burnedReflectionBalance = totalDividend;
-    }
-
-    function getReflectionBalances() public view returns (uint256) {
-        uint256 count = balanceOf(_msgSender());
-        uint256 total = 0;
-        for (uint256 i = 0; count > i; i++) {
-            uint256 tokenId = tokenOfOwnerByIndex(_msgSender(), i);
-            total += getReflectionBalance(tokenId);
-        }
-        return total;
-    }
-
-    function getReflectionBalance(uint256 tokenId)
-        public
-        view
-        returns (uint256)
-    {
-        if (!_exists(tokenId)) return 0;
-        return totalDividend - _lastDividendAt[tokenId];
     }
 
     function claimReward(uint256 tokenId) public nonReentrant {
         require(!isSaleActive(), "BurningZombiesERC721: sale is active");
+
         require(
-            _tokenIdCounter.current() == MAX_SUPPLY,
+            _tokenIdCounter.current() >= MAX_SUPPLY,
             "BurningZombiesERC721: there are unclaimed or not burned tokens"
         );
+
         require(
             _isApprovedOrOwner(_msgSender(), tokenId),
             "BurningZombiesERC721: claim reward caller is not owner nor approved"
@@ -306,10 +319,12 @@ contract BurningZombiesERC721 is
 
     function claimRewards() public nonReentrant {
         require(!isSaleActive(), "BurningZombiesERC721: sale is active");
+
         require(
-            _tokenIdCounter.current() == MAX_SUPPLY,
+            _tokenIdCounter.current() >= MAX_SUPPLY,
             "BurningZombiesERC721: there are unclaimed or not burned tokens"
         );
+
         uint256 count = balanceOf(_msgSender());
         uint256 balance = 0;
 
@@ -320,5 +335,47 @@ contract BurningZombiesERC721 is
         }
 
         Address.sendValue(payable(_msgSender()), balance);
+    }
+
+    /* ========== HELPER FUNCTIONS ========== */
+
+    function _mintTokens(uint256 numberOfTokens, address to) private {
+        for (uint256 i = 0; numberOfTokens > i; i++) {
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+
+            _safeMint(to, tokenId);
+            _minters[tokenId] = to;
+            _lastDividendAt[tokenId] = 0;
+        }
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIextended;
+    }
+
+    function _tokenPrice(uint256 tokenId) private view returns (uint256) {
+        uint256 segmentNo = uint256(uint256(tokenId) / uint256(segmentSize));
+        address sender = _msgSender();
+        uint256 price = (segmentNo * priceStep) + priceBase;
+        uint256 balance = balanceOf(sender);
+
+        return _priceCalculator.getPrice(segmentNo, sender, price, balance);
+    }
+
+    function _splitBalance(uint256 amount) private nonReentrant {
+        uint256 reflectionShare = (amount / 100) * calculateReflectionShare();
+        uint256 mintingShare = amount - reflectionShare;
+
+        _reflectDividend(reflectionShare);
+
+        address payable recipient = payable(_splitter);
+        Address.sendValue(recipient, mintingShare);
+    }
+
+    function _reflectDividend(uint256 amount) private {
+        reflectionBalance += amount;
+        totalDividend += (amount / MAX_SUPPLY);
+        burnedReflectionBalance = totalDividend;
     }
 }
