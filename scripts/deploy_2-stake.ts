edfba8c806de27347302c9b26d87512e59e86115
Talha Akkaya <talha2akkaya@gmail.com>
2022-01-14 10:15:19 -0500

    staking updated

--- scripts/deploy_2-stake.ts
+++ scripts/deploy_2-stake.ts
@@ -2,30 +2,14 @@ import { ethers } from "hardhat";
 import Artifact from "../artifacts/contracts/StakingRewards.sol/StakingRewards.json";
 import { pinJSONToIPFS } from "./utils"; // eslint-disable-line node/no-missing-import
 
-const REWARDS_ADDR = "";
-const STAKING_ADDR = "";
+const REWARDS_ADDR = "0x9c4f88408f9f003Fb10f106E7A69989bB4f3452f";
+const STAKING_ADDR = "0x1b72CFde16E5a33a36eAAFbf2eb9CDEd02B09577";
 
 const main = async () => {
   const stakeFactory = await ethers.getContractFactory("StakingRewards");
   const stake = await stakeFactory.deploy(REWARDS_ADDR, STAKING_ADDR);
 
   await stake.deployed();
-
-  const duration = await stake.setRewardsDuration(31556926);
-  await duration.wait();
-
-  const token = await ethers.getContractAt("BurningZombiesERC20", REWARDS_ADDR);
-  const transfer = await token.transfer(
-    stake.address,
-    ethers.utils.parseUnits("200000", 18)
-  );
-  await transfer.wait();
-
-  const update = await stake.notifyRewardAmount(
-    ethers.utils.parseUnits("200000", 18),
-    { gasLimit: 6000000 }
-  );
-  await update.wait();
 
   const res = await pinJSONToIPFS(Artifact.abi);
 
